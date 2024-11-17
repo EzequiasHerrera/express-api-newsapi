78bf25d2ad922bf3a08376df71d3d2fa85a086cd
ezequiasherrera <ezequiasherrera99@gmail.com>
2024-11-16 23:56:24 -0300

    Changes for Vercel

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import env from 'dotenv/config';
-import Server from "./Server.js";
+import Server from "../Server.js";
 const server = new Server();
 // server.listen();
 export default server.app;
